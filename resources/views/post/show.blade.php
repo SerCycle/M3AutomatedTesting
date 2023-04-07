a133a56b1689ef4b5e6de59502f7954e4c502806
hanstl <hanstarunal@gmail.com>
2023-04-07 22:13:13 +0700

    update show

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,15 +3,15 @@
 @section('content')
 @auth
 <div class="con">
-    <div class=" mx-4 ">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <div class="bg-primary-border-subtle grid-cols-3 gap-3 mx-4">
+        <h2 class="leading-tight ml-3">
             {{ $post->title }}
         </h2>
     </div>
 
     <div class="py-12">
         <div class="col">
-            <div class="card bg-secondary mx-4 ">
+            <div class="card bg-secondary mx-4 shadow">
                 <div class="card">
                     <p class="mx-3 font-base mb-3 mt-3">{{$post->description}}</p>
                     <p class="mx-3 mt-2">{{ $post->created_at->diffForHumans() }}</p>
