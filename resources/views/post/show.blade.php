95c9a8d10c1ddf70c816b909ea7f4e56738a6338
hanstl <hanstarunal@gmail.com>
2023-04-07 22:08:38 +0700

    update show

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -1,18 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+@auth
 <div class="con">
-    <div class="flex justify-between items-center">
+    <div class=" mx-4 ">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ $post->title }}
         </h2>
     </div>
 
     <div class="py-12">
-        <div class="mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    <p class="font-base mb-3 block">{{$post->description}}</p>
-                    <p class="mb-4 mt-2">{{ $post->created_at->diffForHumans() }}</p>
+        <div class="col">
+            <div class="card bg-secondary mx-4 ">
+                <div class="card">
+                    <p class="mx-3 font-base mb-3 mt-3">{{$post->description}}</p>
+                    <p class="mx-3 mt-2">{{ $post->created_at->diffForHumans() }}</p>
                 </div>
             </div>
         </div>
     </div>
 </div>
+
+@endauth
+
+@endsection
